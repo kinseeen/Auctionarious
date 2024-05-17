7a76b4c60972873e11b055d581fa5417f2d37d2a
Kinseeen Kine Jakobsen <kine.jakobsen@hotmail.no>
2024-05-17 21:51:11 +0200

    add register user functionality

--- js/registerUser.mjs
+++ js/registerUser.mjs
@@ -1,7 +1,49 @@
 import { get, put, post } from "./http.mjs";
 
-async function registerUser(name, email, password, avatar) {
+async function registerUser(name, email, password, avatarUrl) {
+ 
   if (!email.endsWith("stud.noroff.no")) {
-    return;
+    return "The email must be of type stud.noroff.no";
   }
+
+  const user = {
+    name,
+    email,
+    password,
+    avatar : {
+      url: avatarUrl,
+    
+
+    },
+  };
+
+  console.log(user);
+return await post("auth/register", user, false);
+
+
 }
+
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    console.log("Registering user...");
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const avatarUrl = document.getElementById("avatar").value;
+    const result = await registerUser(name, email, password, avatarUrl =='' ? "https://via.placeholder.com/300" : avatarUrl);
+    if (typeof result === "string") {
+      document.getElementById("emailError").textContent = result;
+    } 
+    
+    else if (result.errors) {
+      document.getElementById("registrationError").textContent = result.errors[0].message;
+    } else {
+      window.location.href = "/index.html";
+    }
+    
+
+    
+
+  });
